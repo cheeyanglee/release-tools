f1ef82dc045815d3eb273add9537db54ae0a71af
Graydon, Tracy <tracy.graydon@intel.com>
2018-10-19 19:20:15 -0700

    - Added some vars needed by the uninative release stuff - Added some comments - Added some code to make a wild guess at what to use if not on   a host that has an expected domain. Useful for testing.
    Signed-off-by: Graydon, Tracy <tracy.graydon@intel.com>

--- where_am_i.py
+++ where_am_i.py
@@ -8,22 +8,48 @@ __license__ = "GPL"
 __version__ = "2.0"
 __maintainer__ = "Tracy Graydon"
 __email__ = "tracy.graydon@intel.com"
+
+
+We use different paths on the different AB clusters. Figure out what cluster we are on and
+set some paths accordingly. Root path on the yocto.io AB cluster is /srv/autobuilder. For
+"old" cluster, it's /srv/www/vhosts. where_am_i figures it out for us.
+
 '''
 
+import os
 import sys
 import socket
 
 def where_am_i():
     abhost = socket.getfqdn()
+    print "ABHOST: %s" %abhost
     if "yocto.io" in abhost:
         VHOSTS = "/srv/autobuilder"
-    if "autobuilder.yoctoproject.org" in abhost:
+    elif "autobuilder.yoctoproject.org" in abhost:
         VHOSTS = "/srv/www/vhosts"
-    return VHOSTS
+    else:
+        print "I don't know where we are, so I am going to guess..."
+        # Uncomment this if you want to use /srv/www/vhosts as the VHOST dir. It's useful for testing.
+        #VHOSTS = "/srv/www/vhosts"
+        VHOSTS = "/srv/autobuilder"
+        print "Setting VHOSTS to %s" %VHOSTS
+    AB_HOME = os.path.join(VHOSTS, "autobuilder.yoctoproject.org/pub") # uninative release (uninative.py) uses this
+    AB_BASE = os.path.join(AB_HOME, "releases") # all RC candidates live here
+    DL_HOME = os.path.join(VHOSTS, "downloads.yoctoproject.org/releases") # uninative release (uninative.py) uses this
+    DL_BASE = os.path.join(DL_HOME, "yocto") # all other releases use this
+    path_dict = {'VHOSTS': VHOSTS, 'AB_HOME': AB_HOME, 'AB_BASE': AB_BASE, 'DL_HOME': DL_HOME, 'DL_BASE': DL_BASE}
+    return path_dict
 
 if __name__ == '__main__':
 
-    # We use different paths on the different AB clusters. Figure out what cluster we are on and
-    # set the paths accordingly. Root path on the yocto.io AB cluster is /srv/autobuilder. For
-    # "old" cluster, it's /srv/www/vhosts. where_am_i figures it out for us.
-    VHOSTS = where_am_i()
+    PATH_VARS = where_am_i()
+    VHOSTS = PATH_VARS['VHOSTS']
+    AB_HOME = PATH_VARS['AB_HOME']
+    AB_BASE = PATH_VARS['AB_BASE']
+    DL_HOME = PATH_VARS['DL_HOME']
+    DL_BASE = PATH_VARS['DL_BASE']
+    print "VHOSTS: %s" %VHOSTS
+    print "AB_HOME: %s" %AB_HOME
+    print "AB_BASE: %s" %AB_BASE
+    print "DL_HOME: %s" %DL_HOME
+    print "DL_BASE: %s" %DL_BASE
