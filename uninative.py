163acab989c54a987d317439dd5630e518055974
Graydon, Tracy <tracy.graydon@intel.com>
2018-10-19 21:28:33 -0700

    Update script to use the utils.py functions and fix an issue with needing to change back to working dir before calling the unihash.py script.
    Signed-off-by: Graydon, Tracy <tracy.graydon@intel.com>

--- uninative.py
+++ uninative.py
@@ -19,62 +19,8 @@ import os.path
 import shutil
 from shutil import rmtree, copyfile
 from unihash import main
-
-def split_thing(thing, marker):
-    filebits = thing.split(marker)
-    return filebits
-
-def where_am_i():
-    abhost = socket.getfqdn()
-    print "abhost: %s" %abhost
-    if "yocto.io" in abhost:
-        VHOSTS = "/srv/autobuilder"
-    if "autobuilder.yoctoproject.org" in abhost:
-        VHOSTS = "/srv/www/vhosts"
-    else:
-        print "We're lost. Are you on an autobuilder host system? This is not intended to work elsewhere. You'll need to hack your path accordingly."
-        sys.exit()
-    return VHOSTS
-
-def sanity_check(source, target):
-    if not os.path.exists(source):
-       print
-       print "SOURCE dir %s does NOT EXIST." %source
-       print
-       sys.exit()
-    if not os.listdir(source):
-       print
-       print "SOURCE dir %s is EMPTY" %source
-       print
-    if os.path.exists(target):
-       print
-       print "The TARGET directory %s already exists! " %target
-       print
-       sys.exit()
-    return
-
-def sync_it(source, target, exclude_list):
-    print "Syncing %s to %s" %(source, target)
-    source = source + "/"
-    if exclude_list:
-        exclusions = ['--exclude=%s' % x.strip() for x in exclude_list]
-        print "Exclusions: %s" %exclusions
-        print
-        exclude = "--exclude=" + exclude_list[0]
-        length = len(exclude_list)
-        for i in range(1, length):
-            exclude = exclude + " " + "--exclude=" + exclude_list[i]
-        command = 'rsync -avrl ' + str(exclude) + " " + str(source) + " " + str(target)
-        os.system(command)
-    else:
-        os.system("rsync -avrl '%s' '%s'" %(source, target))
-    print
-    return
-
-def get_list(dirname):
-    dirlist = os.listdir(dirname)
-    dirlist.sort()
-    return dirlist
+from where_am_i import where_am_i
+from utils import split_thing, get_list, sanity_check, sync_it
 
 def get_build_hash(source_dir):
     # This looks in the build directory and gets the hash from the poky tarball.
@@ -83,26 +29,22 @@ def get_build_hash(source_dir):
     poky_blob = filter(lambda x: 'poky' in x, files).pop()
     chunks = split_thing(poky_blob, ".")
     hash = split_thing(chunks[0], '-').pop()
+    os.chdir(WORK_DIR)
     return hash
-
 
 if __name__ == '__main__':
 
     os.system("clear")
     print
 
-    # We use different paths on the different AB clusters. Figure out what cluster we are on and
-    # set the paths accordingly. Root path on the yocto.io AB cluster is /srv/autobuilder. For
-    # "old" cluster, it's /srv/www/vhosts. If you need to override, uncomment the next line and
-    # add your own path there, instead of using where_am_i.
-    #VHOSTS = "/path/to/your/stuff"
-    VHOSTS = where_am_i()
-
-    AB_BASE = os.path.join(VHOSTS, "autobuilder.yoctoproject.org/pub/")
-    DL_DIR = os.path.join(VHOSTS, "downloads.yoctoproject.org/releases")
-    DL_BASE = os.path.join(DL_DIR, "uninative")
-    TOOLS_BASE = os.path.join(AB_BASE, "buildtools")
-    LINK_DIR = os.path.join(AB_BASE, "uninative")
+    WORK_DIR = os.getcwd() 
+    PATH_DICT = where_am_i()
+    VHOSTS = PATH_DICT['VHOSTS']
+    AB_HOME = PATH_DICT['AB_HOME']
+    DL_HOME = PATH_DICT['DL_HOME']
+    DL_DIR = os.path.join(DL_HOME, "uninative")
+    TOOLS_BASE = os.path.join(AB_HOME, "buildtools")
+    LINK_DIR = os.path.join(AB_HOME, "uninative")
 
     CRUFT_LIST = ['*.md5sum', '*.json'] # We don't want to copy this stuff for a release.
 
@@ -123,7 +65,7 @@ if __name__ == '__main__':
         print "REL_ID: %s\n" %REL_ID
         
         SOURCE = os.path.join(TOOLS_BASE, BUILD_ID)
-        RELEASE_DIR = os.path.join(DL_BASE, REL_ID)
+        RELEASE_DIR = os.path.join(DL_DIR, REL_ID)
         sanity_check(SOURCE, RELEASE_DIR)
         TOOLS_DIR = os.path.join(SOURCE, "toolchain")
     else:
@@ -133,7 +75,8 @@ if __name__ == '__main__':
 
     # Go find the poky commit that we built for the uninative release.
     POKY_HASH = get_build_hash(SOURCE)
-
+    print "POKY_HASH: %s" %POKY_HASH
+    
     # Make the dowloads directory and copy the stuff over from build dir
     tools_list = get_list(TOOLS_DIR)
     for dirname in tools_list:
