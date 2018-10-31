10e33df2dd9e2c0b830c2b4e7d4add97ef96d6df
Tracy Graydon <tracy.graydon@intel.com>
2018-10-30 19:38:37 -0700

    /release-tools/meta-notes.py
    - Read in the security info for the CVE/Security section of the
    meta-intel release notes.
    
    - Rewrite to use pygit2 instead of os system commands for repo
    management. Previous method sometimes failed to update the repo
    correctly, resulting in bogus content for Fixes section. This fixes
    that problem.
    
    Signed-off-by: Tracy Graydon <tracy.graydon@intel.com>

--- meta-notes.py
+++ meta-notes.py
@@ -1,33 +1,33 @@
-'''
-Created on Feb 22, 2018
-
-__author__ = "Tracy Graydon"
-__copyright__ = "Copyright 2018, Intel Corp."
-__credits__ = ["Tracy Graydon"]
-__license__ = "GPL"
-__version__ = "2.0"
-__maintainer__ = "Tracy Graydon"
-__email__ = "tracy.graydon@intel.com"
-'''
-
 import os
+import os.path
 import optparse
 import sys
-import hashlib
-import glob
-import os.path
-import shutil
-from sh import git
-from shutil import rmtree, copyfile
-from subprocess import call, Popen
+import re
+from shutil import rmtree
+from pygit2 import Repository, clone_repository
+from pygit2 import GIT_SORT_TIME
+from utils import where_am_i, split_thing, rejoin_thing
 
-def split_thing(thing, marker):
-    filebits = thing.split(marker)
-    return filebits
 
-def rejoin_thing(thing, marker):
-    filebits = marker.join(thing)
-    return filebits
+def get_repo(codename):
+    repo_url = 'https://git.yoctoproject.org/git/meta-intel'
+    CWD = os.getcwd()
+    repo_path = os.path.join(CWD,'meta-intel')
+    if os.path.exists(repo_path):
+        print "\nFound an existing meta-intel repo. Nuking it."
+        rmtree(repo_path)
+    print "Cloning the meta-intel repo."
+    # If you pass in a non-existent branch, this will fail spectacularly. So let's catch that and die gracefully.
+    try:
+        meta_repo =clone_repository(repo_url, repo_path, checkout_branch=codename)
+    except:
+        print "Couldn't check out the meta-intel repo with branch %s. Check the branch name you passed in." %codename
+        sys.exit()
+    # Are we where we think we are?
+    head = meta_repo.head
+    branch_name = head.name
+    print "We are now on branch: %s\n" %branch_name
+    return meta_repo
 
 def release_type(build_id):
     RC = split_thing(build_id, ".")[-1].lower()
@@ -82,10 +82,6 @@ if __name__ == '__main__':
     os.system("clear")
     print
 
-    VHOSTS = "/srv/www/vhosts"
-    AB_BASE = os.path.join(VHOSTS, "autobuilder.yoctoproject.org/pub/releases")
-    DL_BASE = os.path.join(VHOSTS, "downloads.yoctoproject.org/releases/yocto")
-
     parser = optparse.OptionParser()
     parser.add_option("-i", "--build-id",
                       type="string", dest="build",
@@ -93,17 +89,28 @@ if __name__ == '__main__':
     parser.add_option("-r", "--revisions",
                       type="string", dest="revs",
                       help="Required. Specify the revision range to use for the git log. i.e. meta-intel-8.1-rocko-2.4.2 would use 8.0-rocko-2.4..HEAD.")
-
     (options, args) = parser.parse_args()
-
+    
     if not (options.build and options.revs):
         print "You must specify the RC and revision range."
         print "Please use -h or --help for options."
         sys.exit()
-    
+  
+    PATH_VARS = where_am_i()
+    VHOSTS = PATH_VARS['VHOSTS']
+    AB_HOME = PATH_VARS['AB_HOME']
+    AB_BASE = PATH_VARS['AB_BASE']
+    DL_HOME = PATH_VARS['DL_HOME']
+    DL_BASE = PATH_VARS['DL_BASE']
+    print "VHOSTS: %s" %VHOSTS
+    print "AB_HOME: %s" %AB_HOME
+    print "AB_BASE: %s" %AB_BASE
+    print "DL_HOME: %s" %DL_HOME
+    print "DL_BASE: %s" %DL_BASE
+    print
     print "Build ID: %s" %options.build
     print "Revisions: %s" %options.revs
-    
+    print
 
     REVISIONS = options.revs
     VARS = release_type(options.build)
@@ -121,13 +128,10 @@ if __name__ == '__main__':
     RELEASE_NOTES = ".".join(["RELEASE_NOTES", RELEASE])
     META_TARBALL = RELEASE + ".tar.bz2"
     MD5FILE = META_TARBALL + ".md5sum"
-    DL_BASE = "http://downloads.yoctoproject.org/releases/yocto"
-    MIRROR_BASE = "http://mirrors.kernel.org/yocto/yocto"
+    DL_URL = "http://downloads.yoctoproject.org/releases/yocto"
+    MIRROR_URL = "http://mirrors.kernel.org/yocto/yocto"
     HOME = os.getcwd()
-    META_REPO = os.path.join(HOME, "meta-intel")
-    GIT_LOG = os.path.join(HOME, "git_log.txt")
-    FIXES = os.path.join(HOME, "FIXES")
-    CVE = os.path.join(HOME, "CVE")
+    SEC_INFO = os.path.join(HOME, "meta-intel-sec-info.txt")
 
     # Running through the dictionary to print var values results in random order, making it hard to read. So loop through values in desired order.
     for thing in ['RC_DIR', 'RELEASE', 'REL_TYPE', 'META_VER', 'YP_VER', 'YP_NAME', 'RC', 'BRANCH', 'TAG', 'RC_SOURCE', 'RELEASE_DIR']:
@@ -136,12 +140,25 @@ if __name__ == '__main__':
     print "META_TARBALL: %s" %META_TARBALL
     print "MD5FILE: %s" %MD5FILE
 
+    # Now we have branch name and stuff, so let's clone the repo
+    repo = get_repo(BRANCH)
+
+    # Make sure the starting revision/tag exists.
+    rev_chunks = split_thing(REVISIONS, "..")
+    tag_rev = rev_chunks[0]  # The tag of the previous release
+    head_rev = rev_chunks[1] # This is usually going to be the HEAD of the release branch we are on
+    regex = re.compile('^refs/tags')
+    tag_list = filter(lambda r: regex.match(r), repo.listall_references())
+    if not tag_rev in str(tag_list):
+        print "I can't find a tag matching %s. Check your revisions." %tag_rev
+        sys.exit()
+
     # Set up the release notes file
     outpath = os.path.join(HOME, RELEASE_NOTES)
     outfile = open(outpath, 'w')
     outfile.write("\n----------------------\n%s Errata\n----------------------\n\n" %TAG)
     os.chdir(RELEASE_DIR)
-    
+
     # Hash file is a symlink to the blob. So find the symlink
     for root, dirs, files in os.walk(RELEASE_DIR, topdown=True):
         for name in files:
@@ -163,8 +180,8 @@ if __name__ == '__main__':
     blob = md5line[1]
     print "blob: %s" %blob
     f.close()
-    DL_URL = "/".join([DL_BASE, YP_NAME, blob]).strip()
-    MIRROR_URL = "/".join([MIRROR_BASE, YP_NAME, blob]).strip()
+    DL_URL = "/".join([DL_URL, YP_NAME, blob]).strip()
+    MIRROR_URL = "/".join([MIRROR_URL, YP_NAME, blob]).strip()
     outfile.write("Release Name: %s\n" %RELEASE)
     outfile.write("Branch: %s\n" %BRANCH)
     outfile.write("Tag: %s\n" %TAG)
@@ -179,27 +196,32 @@ if __name__ == '__main__':
     outfile.write("\n--------------\n Known Issues\n--------------\n")
     outfile.write("N\A\n\n")  # If there are known issues to add, we do that manually later
     os.chdir(HOME)
-    print "Cloning the meta-intel repo."
-    if not os.path.exists(META_REPO):
-        git.clone('ssh://git@git.yoctoproject.org/meta-intel')  
-    os.chdir(META_REPO)
-    git.fetch("origin")
-    print "Checking out %s branch." %BRANCH
-    git.checkout(BRANCH)
-    format_string = '\"%s\"'
-    os.system('git log --pretty=format:%s %s > %s' %(format_string, REVISIONS, GIT_LOG))
-    os.chdir(HOME)
-    print "Getting Security Fixes."
+
+    print "Writing out the Security Info."
     outfile.write("\n---------------\nSecurity Fixes\n---------------\n")
-    with open(GIT_LOG, 'r') as gitlog:
-        lines = gitlog.readlines()
-    for line in lines:
-        if "CVE" in line:
-            outfile.write(line)
+    if os.path.isfile(SEC_INFO):
+        infofile = open(SEC_INFO, 'r')
+        all_the_things = infofile.read()
+        infofile.close()
+        outfile.writelines(all_the_things)
+
+    print "Getting the Fixes for this release."
     outfile.write("\n\n---------------\nFixes\n---------------\n")
-    for line in lines:
-        if not "CVE" in line:
-            outfile.write(line)
-    gitlog.close()
+
+    start = repo.revparse_single(head_rev)
+    release_tag = repo.revparse_single(tag_rev)
+
+    print "Start ID: %s" %start.id
+    print "Release_tag: %s" %release_tag.id
+
+    walker = repo.walk(start.id, GIT_SORT_TIME)
+    walker.hide(release_tag.id)
+    for commit in walker:
+        commit_id = str(commit.id)
+        raw_message = str(commit.message.rstrip())
+        # print "%s: %s" %(commit_id[0:8], raw_message.splitlines()[0])
+        # If you want to include the commit hash, uncomment this one.
+        #outfile.write("%s: %s\n" %(commit_id[0:8], raw_message.splitlines()[0]))
+        outfile.write("%s\n" %raw_message.splitlines()[0])
     outfile.close()
     print "DONE!"
